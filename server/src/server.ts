97a8bb21540f0f38f993fa6f518c9059257bbf81
Aryan Verma <102582703+aryanverma2506@users.noreply.github.com>
2023-05-18 00:00:31 +0530

    Adding Share Place MERN Project to Git

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
-import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 import mongooseConnect from "./config/database";
 import placesRoutes from "./routes/places-routes";
 import usersRoutes from "./routes/users-routes";
-// import cors from "./config/cors";
+import cors from "./config/cors";
 import * as errorControllers from "./controllers/error-controllers";
 
 const app = express();
@@ -21,13 +20,7 @@ const allowedOrigins = process.env.CLIENT_URLS?.split(",").map((url) =>
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
-// app.use(cors({ origin: allowedOrigins }));
+app.use(cors({ origin: allowedOrigins }));
 
 app.use(
   "/shared/uploads",
